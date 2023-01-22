a2614c73ec01b19a2df829aae7d861746d2b6e73
pgotev <plamen.gotev@gmail.com>
2023-01-22 23:42:54 +0200

    first steps with the game
    Welcome to the game by a simple yes/no question and then the player picks. I lot of text trying to be entertaining and engaging but very simple functionalities at this point really.

---
+++ rock_paper_scissors.py
@@ -0,0 +1,69 @@
+import random
+# First we make sure the player wants to start playing or not
+print('Are you ready for the most exiting game of Rock, Paper, Scissors ever? Y/N')
+ready_or_not = input('Type in your answer here. [Y]es or [N]o? ').lower()
+mistake_counter = 0
+
+while True:
+    if ready_or_not == 'y' or ready_or_not == 'yes':
+        print('OK. Lets begin!')
+        print('------------------------------------------')
+        break
+    elif ready_or_not == 'n' or ready_or_not == 'no':
+        print('OK, but you\'re missing out. Goodbye.')
+        exit()
+    else:
+        if mistake_counter == 0:
+            print('This is an invalid command. I hope this was an honest mistake and you\'re not trying to test my'
+                  ' capabilities. Just type Y to start playing or N to exit the program: ', end='')
+            mistake_counter += 1
+            ready_or_not = input().lower()
+        elif mistake_counter == 1:
+            print('This is your second invalid command. If you want to play you have to follow my commands. Type in Y to start or N to exit: ', end='')
+            mistake_counter += 1
+            ready_or_not = input().lower()
+        elif mistake_counter == 2:
+            print('This is your third invalid command. You get one more chance but after that it\'s over. I mean it. ', end='')
+            mistake_counter += 1
+            ready_or_not = input().lower()
+        elif mistake_counter > 2:
+            print('I wasn\'t bluffing. END GAME')
+            exit()
+
+print('To pick just type in the first letter of your choice:')
+print('[R]: Rock')
+print('[P]: Paper')
+print('[S]: Scissors')
+print()
+print('For detailed explanation of the rules of the game type in: Help')
+
+players_choice_input = input('What will it be: ').lower()
+print('------------------------------------------')
+
+if players_choice_input == 'help' or players_choice_input == 'h':
+    print('You dont know the rules of Rock, Paper, Scissors? Really? OK, here you go: Rock beats scissors, scissors beat paper and paper beats rock.')
+    players_choice_input = input('Now that you know all about the game, make your pick already: Rock, Paper or Scissors? ').lower()
+
+mistake_counter = 0
+while True:
+    if players_choice_input == 'r':
+        players_choice = 'Rock'
+        break
+    elif players_choice_input == 'p':
+        players_choice = 'Paper'
+        break
+    elif players_choice_input == 's':
+        players_choice = 'Scissors'
+        break
+    else:
+        if mistake_counter == 0:
+            print('Invalid command. You have one more chance:')
+            mistake_counter += 1
+            players_choice_input = input('Type in R for Rock, P for Paper or S for Scissors: ')
+        else:
+            print('Invalid command.')
+            raise SystemExit()
+
+print(f'You have chosen: {players_choice} This is a wise choice. It\'s proven to be one of the Top 3 best choices you '
+      f'can make. You\'re clearly good at this game, but let\'s see what your mighty opponent has prepared for you.')
+
